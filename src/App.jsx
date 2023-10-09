33287313a462ab7fc26f8daba14bae4023dc1d4d
David Andersson <david.andersson@zocom.se>
2023-10-09 15:34:12 +0200

    Visibility demo

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,32 @@ import './App.css'
 
 function App() {
 	const [chameleonToggled, setChameleonToggled] = useState(false)
+	const [isVisible, setIsVisible] = useState(true)
 
 	const chameleonClass = 'chameleon ' + (chameleonToggled ? 'toggled' : '')
+	const visibleClass = 'visibility-container ' + (isVisible ? '' : 'invisible')
 
 	const chameleonClick = () => setChameleonToggled(!chameleonToggled)
+	const handleVisibility = () => setIsVisible(!isVisible)
 
 	return (
 		<main>
 		
-			<div>
+			<div className="chameleon-container">
 				<button onClick={chameleonClick}>
 					Change color
 				</button>
 				<div className={chameleonClass}></div>
 			</div>
+
+			<div className={visibleClass}>
+				<div> visibility </div>
+				<div> opacity </div>
+				<div> display </div>
+				{isVisible ? <div> c.rendering </div> : null}
+			</div>
+			<br/>
+			<button onClick={handleVisibility}> Toggle visibility </button>
 
 		</main>
 	)
